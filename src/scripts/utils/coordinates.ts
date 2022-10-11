f222440fb73fdc06a83d3071a967d5afe31ad897
Romain <romain.neuplanche@epitech.eu>
2022-10-11 11:03:47 +0200

    Fix coordinates

--- src/scripts/utils/coordinates.ts
+++ src/scripts/utils/coordinates.ts
@@ -1,10 +1,10 @@
 const ddToDmsHelper = (dd: number, isLat: boolean) => {
-    const deg = Math.floor(dd)
-    const min = Math.floor((dd - deg) * 60)
-    const sec = ((dd - deg - min / 60) * 3600).toFixed(4)
+    const deg = dd > 0 ? Math.floor(dd) : Math.ceil(dd);
+    const min = Math.floor((Math.abs(dd) - Math.abs(deg)) * 60);
+    const sec = (Math.abs(dd) - Math.abs(deg) - min / 60) * 3600;
     const dir = isLat ? (dd >= 0 ? 'N' : 'S') : (dd >= 0 ? 'E' : 'W')
 
-    return `${deg}°${min}'${sec}"${dir}`
+    return `${deg}°${min}'${sec.toFixed(4)}"${dir}`
 }
 
 export const ddToDms = (coordinates: { lng: number, lat: number }): { lng: string, lat: string } => {

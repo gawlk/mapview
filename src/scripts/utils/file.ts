eeb90280352cfb9a5ddec5982443d57179ea9d2a
A.OUDINOT <antoine.oudinot@isaan.fr>
2023-02-09 10:49:35 +0100

    #266 - prepare check for file name

--- src/scripts/utils/file.ts
+++ src/scripts/utils/file.ts
@@ -1,3 +1,13 @@
+export const reservedWords = (() => {
+  const words = ['CON', 'PRN', 'AUX', 'NUL']
+
+  for (let i = 1; i < 9; i++) {
+    words.push(`COM${i}`, `LPT${i}`)
+  }
+
+  return words
+})()
+
 export const convertFileToDataURL = (file: File) =>
   new Promise<string>((resolve, reject) => {
     const reader = new FileReader()
@@ -40,11 +50,22 @@ export const convertData64ImageToUint8Array = async (data64: string) => {
 export const downloadImage = async (screenshot: string) =>
   downloadFile(await convertData64ImageToFile(screenshot))
 
+export function isValidFileNAme(name: string) {
+  return (
+    name.length > 0 &&
+    !/[\\\|\/*<>:"?\x00-\x1F]/.test(name) &&
+    reservedWords.includes(name.toUpperCase())
+  )
+}
+
 export const downloadFile = async (file: File) => {
+  console.log('file', file)
   const a = document.createElement('a')
 
   a.href = URL.createObjectURL(file)
   a.download = file.name
+
+  console.log('a', a)
 
   a.target = '_blank' // Needed for Safari
   a.click()

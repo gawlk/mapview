1ca65534632d52cb6fa4e91a0fd066cfbd468bc5
Kevin Gawlik <kevin@gawlik.io>
2023-03-01 16:21:52 +0100

    fix: issues in !56

--- src/scripts/entities/data/label/index.ts
+++ src/scripts/entities/data/label/index.ts
@@ -1,7 +1,5 @@
 import { translate } from '/src/locales'
 
-import { rawCategory, currentCategory } from '/src/scripts'
-
 export const createDataLabel = <T extends string, Unit extends string>(args: {
   name: string
   shortName?: string

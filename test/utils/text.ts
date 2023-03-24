15053af13bebc8c4f57badf10c951796f4eac9fd
A.OUDINOT <antoine.oudinot@isaan.fr>
2023-03-24 13:59:43 +0100

    F #229 - prepare parser

--- test/utils/text.ts
+++ test/utils/text.ts
@@ -22,3 +22,27 @@ export const fileToStringArray = async (
 export const getKey = (ligne: string) => {
   return ligne.split('=')[0].trim()
 }
+
+export const parseData: any = (data: string) => {
+  const trimedData = data.trim()
+
+  if (trimedData === 'undefined') {
+    return undefined
+  }
+
+  if (trimedData.match(/^[0-9]+$/) !== null) {
+    return Number(trimedData)
+  }
+
+  const timestamp = Date.parse(data)
+
+  if (!isNaN(timestamp)) {
+    return new Date(timestamp)
+  }
+
+  if (trimedData.includes(',')) {
+    return trimedData.split(',').map((splittedDate) => parseData(splittedDate))
+  }
+
+  return data
+}

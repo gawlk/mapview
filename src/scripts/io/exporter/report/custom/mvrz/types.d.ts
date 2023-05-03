ce0cd45c99d3a436aece523cea95fa6cd59f2085
OUDINOT Antoine <antoine.oudinot@isaan.fr>
2023-05-03 14:16:49 +0200

    #404 - restore ExcelFlatData

--- src/scripts/io/exporter/report/custom/mvrz/types.d.ts
+++ src/scripts/io/exporter/report/custom/mvrz/types.d.ts
@@ -2,4 +2,6 @@ type ExcelData = string | number | boolean | null
 
 type ExcelJSON = Record<string, ExcelData | ExcelData[]>
 
+type ExcelFlatData = string[] | number[] | boolean[]
+
 type FlatDataJson = Record<string, ExcelFlatData>

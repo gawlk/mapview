058f440117d280bbdcb16e6b22660d92ae831e5e
gawlk <kevin@gawlik.io>
2023-05-09 11:07:22 +0200

    netlify: fix /index import 2

--- src/routes/index/components/bar/components/menuProject/index.tsx
+++ src/routes/index/components/bar/components/menuProject/index.tsx
@@ -2,7 +2,7 @@ import { useI18n } from '@solid-primitives/i18n'
 
 import ButtonAddProject from './components/buttonAddProject'
 import ButtonSave from './components/buttonSave'
-import DialogConfigurations from './components/dialogConfigurations'
+import DialogConfigurations from './components/dialogConfigurations/index.tsx'
 import DialogInformations from './components/dialogInformations'
 import DialogRemoveProject from './components/dialogRemoveProject'
 import SelectProjects from './components/selectProjects'

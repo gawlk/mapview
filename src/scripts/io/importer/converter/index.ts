bdd8be140f715b47bc9ba33108430b802f58423c
Kevin Gawlik <kevin@gawlik.io>
2023-09-06 14:41:32 +0000

    fix: #501

--- src/scripts/io/importer/converter/index.ts
+++ src/scripts/io/importer/converter/index.ts
@@ -8,10 +8,26 @@ import {
 export const convertJSONFromPRJZToMPVZ = (json: RecordAny) => {
   let machine: MachineName = 'Minidyn'
 
-  if (json.Database.Software === 'Fwddyn') {
-    machine = 'Heavydyn'
-  } else if (json.Hardware.Serial.split('-')[0] === 'MAX') {
-    machine = 'Maxidyn'
+  const pcb = Object.keys(json.Hardware).find((key) =>
+    key.toLowerCase().includes('TypeMachinePCB'.toLowerCase()),
+  )
+
+  if (pcb) {
+    switch (json.Hardware[pcb]) {
+      case 0xa981c52c:
+      case 0x0e418beb:
+      case 0xb0a4cf72:
+      case 0x3b599706: {
+        machine = 'Maxidyn'
+        break
+      }
+      case 0x7b33a869:
+      case 0xe37d6725:
+      case 0x5b2c3a47: {
+        machine = 'Heavydyn'
+        break
+      }
+    }
   }
 
   const baseProject = convertPRJZToBaseProject(json, machine)

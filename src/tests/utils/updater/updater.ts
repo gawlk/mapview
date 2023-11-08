1db467b9c8ced92e8b157943753df4c2ea05c018
OUDINOT Antoine <mrdiablon@outlook.fr>
2023-11-08 10:18:28 +0100

    #547 - prepare update by file

--- src/tests/utils/updater/updater.ts
+++ src/tests/utils/updater/updater.ts
@@ -1,7 +1,15 @@
 import fs, { statSync } from 'fs'
 import path from 'path'
 import { program } from 'commander'
-import { heavydynF25Exporter, importFile } from 'dist/main'
+import {
+  heavydynDynatestExporter,
+  heavydynF25Exporter,
+  heavydynPDXExporter,
+  heavydynSwecoExporter,
+  importFile,
+  mpvzExporter,
+  mvrzExporter,
+} from 'dist/main'
 
 // const _filename = fileURLToPath(import.meta.url)
 // const _dirname = path.dirname(_filename)
@@ -28,22 +36,32 @@ if (typeof File === 'undefined') {
   }
 }
 
-program
-  .name('updater')
-  .description('CLI to update reference file of the CI')
-  .version('0.0.1')
-  .arguments('<path>')
-  .parse()
+// program
+//   .name('updater')
+//   .description('CLI to update reference file of the CI')
+//   .version('0.0.1')
+//   .arguments('<path>')
+//   .option('--test')
+//   .parse()
+
+const writeFile = async (filePath: string, file: File) => {
+  fs.writeFileSync(filePath, new DataView(await file.arrayBuffer()))
+}
 
 const updateRefFile = async () => {
   // const options = program.opts()
   const arg = program.args
   const currentPath = arg[0]
 
+  console.log(currentPath, arg, process.argv)
+
   const dirPath = path.dirname(currentPath)
+  const name = path.basename(currentPath)
   const extension = String(path.extname(currentPath))
     .toLowerCase()
     .replace('.', '')
+
+  console.log(name, extension)
 
   const folders = fs.readdirSync(dirPath)
 
@@ -64,22 +82,43 @@ const updateRefFile = async () => {
     return
   }
 
-  const mpvzBlob = new Blob([fs.readFileSync(subPath)])
+  const projectBlob = new Blob([fs.readFileSync(subPath)])
 
-  const mpvzFile = new File([await mpvzBlob.arrayBuffer()], mpvzFileName)
+  const projectFile = new File([await projectBlob.arrayBuffer()], mpvzFileName)
 
-  const project = await importFile(mpvzFile)
+  const project = (await importFile(projectFile)) as HeavydynProject
 
-  // console.log(extension)
+  project?.addToMap()
+
+  let exportedFile
 
   switch (extension) {
     case 'f25':
-      {
-        const f25 = heavydynF25Exporter.export(project as HeavydynProject)
-        console.log(await f25.text())
+      exportedFile = heavydynF25Exporter.export(project)
+      break
+    case 'fwd':
+      if (name.includes('sweco')) {
+        exportedFile = heavydynSwecoExporter.export(project)
+      } else if (name.includes('dynatest')) {
+        exportedFile = heavydynDynatestExporter.export(project)
       }
       break
+    case 'pdx':
+      exportedFile = heavydynPDXExporter.export(project)
+      break
+    case 'mvrz':
+      exportedFile = await mvrzExporter.export(project)
+      break
+    case 'mpvz':
+      exportedFile = await mpvzExporter.export(project)
   }
+
+  if (!exportedFile) {
+    console.error(`file extension not managed ${extension}`)
+    return
+  }
+
+  await writeFile(currentPath, exportedFile)
 }
 
 void updateRefFile()

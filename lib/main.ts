99708b11e0469eb6154333e0455afdd821eed7df
OUDINOT Antoine <mrdiablon@outlook.fr>
2023-11-15 17:33:59 +0100

    #547 - fix build error

--- lib/main.ts
+++ lib/main.ts
@@ -1,14 +1,13 @@
 import {
-  getFilesFromDir,
   heavydynDynatestExporter,
   heavydynF25Exporter,
   heavydynPDXExporter,
   heavydynSwecoExporter,
   importFile,
-  loadMpvz,
   mpvzExporter,
   mvrzExporter,
 } from '/src/scripts'
+import { getFilesFromDir, loadMpvz } from '/src/scripts/io/tests'
 
 export {
   getFilesFromDir,
--- src/scripts/io/index.ts
+++ src/scripts/io/index.ts
@@ -1,3 +1,2 @@
 export * from './exporter'
 export * from './importer'
-export * from './tests'
